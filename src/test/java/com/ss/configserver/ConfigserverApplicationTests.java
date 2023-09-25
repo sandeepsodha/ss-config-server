e7bd4570023ec3088d7f7e4b1586bb85a325f3b9
sandeepsodha <41476359+sandeepsodha@users.noreply.github.com>
2023-09-25 19:06:16 +0530

    Second final commit

---
+++ src/test/java/com/ss/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,10 @@
+package com.ss.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+
+}
